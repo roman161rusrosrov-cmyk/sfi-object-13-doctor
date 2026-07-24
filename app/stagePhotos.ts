b5620daaa676d3e5e57e2489ac09e335c3c94d58
roman161rusrosrov-cmyk <roman161rusrosrov@gmail.com>
2026-07-24 12:43:38 +0300

    Собрать фотографии всех 13 стадий

---
+++ app/stagePhotos.ts
@@ -0,0 +1,31 @@
+import { STAGE_PHOTO_01 } from "./stagePhoto01";
+import { STAGE_PHOTO_02 } from "./stagePhoto02";
+import { STAGE_PHOTO_03 } from "./stagePhoto03";
+import { STAGE_PHOTO_04 } from "./stagePhoto04";
+import { STAGE_PHOTO_10 } from "./stagePhoto10";
+import {
+  STAGE_PHOTO_05,
+  STAGE_PHOTO_06,
+  STAGE_PHOTO_07,
+  STAGE_PHOTO_08,
+  STAGE_PHOTO_09,
+  STAGE_PHOTO_11,
+  STAGE_PHOTO_12,
+  STAGE_PHOTO_13,
+} from "./stagePhotosRemaining";
+
+export const STAGE_PHOTOS = [
+  STAGE_PHOTO_01,
+  STAGE_PHOTO_02,
+  STAGE_PHOTO_03,
+  STAGE_PHOTO_04,
+  STAGE_PHOTO_05,
+  STAGE_PHOTO_06,
+  STAGE_PHOTO_07,
+  STAGE_PHOTO_08,
+  STAGE_PHOTO_09,
+  STAGE_PHOTO_10,
+  STAGE_PHOTO_11,
+  STAGE_PHOTO_12,
+  STAGE_PHOTO_13,
+] as const;
